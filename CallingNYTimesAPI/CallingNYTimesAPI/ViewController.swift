b77c76eda5379e2d5679f07a755a241c8c71885b
Sara <saraahmad1114@yahoo.com>
2016-10-17 14:39:56 -0400

    done

--- CallingNYTimesAPI/CallingNYTimesAPI/ViewController.swift
+++ CallingNYTimesAPI/CallingNYTimesAPI/ViewController.swift
@@ -13,6 +13,13 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        NewYorkTimesAPIClient.getBestSellerInformationWith { (arrayOfDictionary) in
+            print("**************************")
+            print(arrayOfDictionary)
+            print("**************************")
+
+        }
     }
 
     override func didReceiveMemoryWarning() {
